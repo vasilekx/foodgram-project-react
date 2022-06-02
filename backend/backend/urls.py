3e68e7f36b52194270dc83f5d1f9e7db97dc1a18
Vladislav Vasilenko <vasilekx8@yandex.ru>
2022-06-02 16:51:16 +0300

    Debugging a project

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,6 @@
 # backend/urls.py
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
